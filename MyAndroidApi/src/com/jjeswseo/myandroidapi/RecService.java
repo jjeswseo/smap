994d8f12eaa0405273a30f41087aa4a4958750df
jjeswseo <jjeswseo@gmail.com>
2014-06-20 17:47:30 +0900

    filesave

--- MyAndroidApi/src/com/jjeswseo/myandroidapi/RecService.java
+++ MyAndroidApi/src/com/jjeswseo/myandroidapi/RecService.java
@@ -2,23 +2,25 @@ package com.jjeswseo.myandroidapi;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import android.app.Service;
 import android.content.Intent;
 import android.database.Cursor;
-import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaRecorder;
 import android.os.Environment;
 import android.os.IBinder;
 import android.provider.CallLog;
 import android.util.Log;
-import android.widget.Toast;
 
 public class RecService extends Service {
 	String TAG = "RecService";
 	MediaRecorder mMediaRecorder = null;
-	String path = null;
+	String tmpFileName = null;
+	String tmpDir = null;
+	String date = null;
+	private ExecutorService mThreadPool;
 	 
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -33,19 +35,21 @@ public class RecService extends Service {
 		}else{
 			mMediaRecorder.reset();
 		}
+		mThreadPool = Executors.newCachedThreadPool();
 	}
 	@Override
 	public int onStartCommand(Intent intent, int flag, int startId){
 		
 		String ext = Environment.getExternalStorageState();
 		if (ext.equals(Environment.MEDIA_MOUNTED)) {
-			path = Environment.getExternalStorageDirectory().getAbsolutePath();
+			tmpDir = Environment.getExternalStorageDirectory().getAbsolutePath();
 		}
-		path += File.separator+"test.3gp";
+		tmpFileName = "test.3gp";
+		
 		mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.VOICE_CALL);
 		mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
 		mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-		mMediaRecorder.setOutputFile(path);
+		mMediaRecorder.setOutputFile(tmpDir+File.separator+tmpFileName);
 		try {
 			mMediaRecorder.prepare();
 		} catch (IllegalStateException e) {
@@ -68,9 +72,19 @@ public class RecService extends Service {
 		mCursor.moveToFirst();
 		String number = mCursor.getString(mCursor.getColumnIndex(CallLog.Calls.NUMBER));
 		String type = mCursor.getString(mCursor.getColumnIndex(CallLog.Calls.TYPE));
-		String date = mCursor.getString(mCursor.getColumnIndex(CallLog.Calls.DATE));
+		date = mCursor.getString(mCursor.getColumnIndex(CallLog.Calls.DATE));
 		Log.i(TAG, "number,type,date["+number+"]["+type+"]["+date+"]");
 		mCursor.close();
+		mThreadPool.execute(new Runnable() {
+			@Override
+			public void run() {
+				Log.i(TAG, "Temp File["+tmpDir+File.separator+tmpFileName+"]");
+				File tmpFile = new File(tmpDir+File.separator+tmpFileName);
+				File saveFile = new File(tmpDir+File.separator+date+".3gp");
+				tmpFile.renameTo(saveFile);
+				Log.i("RecService[Runnable]","Saved File ["+tmpDir+File.separator+date+".3gp]");
+			}
+		});
 		
 		
 	}
